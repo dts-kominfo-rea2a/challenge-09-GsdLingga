44f44a7869bf9a62963b04efc403a71ea88c3822
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2022-06-21 15:37:55 +0800

    feat: show Contact and make CSS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  console.log(data);
+  return (
+    <div class="contact">
+      <img class="photo" src={data.photo}></img>
+      <div class="profile">
+        <p>{data.name}</p>
+        <p>{data.phone}</p>
+        <p>{data.email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
